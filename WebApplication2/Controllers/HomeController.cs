e07a27efe89656f67c8358e0a64ddf8c00b13b7f
coldcoder <727933158@qq.com>
2019-03-07 12:19:31 +0800

    update

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication2.Models;
-using Library.Interface;
 
 namespace WebApplication2.Controllers
 {
